9bbadb94c67ec0e0b61348fb907c5a3e020e6095
Topper Bowers <topper@toppingdesign.com>
2022-04-11 14:00:08 +0100

    constants

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,8 @@ const mainnetAddresses = {
   "community_pool_address": "0xb2BadB7f28075CB2C8BDBd730204750Db4C03f98",
   "deposit_box_eth_address": "0x9910cF6ba22915C5afCe8b682f7c09d1c001FA59",
   "deposit_box_erc20_address": "0xb3bf0c62f0924e5C8fdae9815355eA98Fba33C8E",
-
+  "deposit_box_erc721_address": "0x98937f91885dcCfF8082623a157296AA161a9917",
+  "deposit_box_erc1155_address": "0xa0EF1521f56641F9E0E43c46E0F6B20715E454c8",
 }
 
 export default mainnetAddresses
